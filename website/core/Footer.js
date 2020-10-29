4f06c41edd45e65f4af6bc4df87ee2ce4c312cfd
Brian Warner <brian@bdwarner.com>
2020-10-29 13:08:10 -0400

    Updates to footer links
    Signed-off-by: Brian Warner <brian@bdwarner.com>

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -24,13 +24,14 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <section className="copyright">Copyright &copy; The Presto Foundation.
-All rights reserved. The Linux Foundation has registered trademarks and uses
-trademarks. For a list of trademarks of The Linux Foundation, please see our <a
-href="https://www.linuxfoundation.org/trademark-usage">Trademark Usage</a> page.
-Linux is a registered trademark of Linus Torvalds. <a
-href="http://www.linuxfoundation.org/privacy">Privacy Policy</a> and <a
-href="http://www.linuxfoundation.org/terms">Terms of Use</a>.</section>
+        <section className="copyright">
+          Copyright &copy; The Presto Foundation.<br />
+          All rights reserved. Presto is a registered trademark of LF Projects, LLC. <br />Please see our
+          <a href="https://lfprojects.org/policies/trademark-policy/">Trademark Policy</a> for more
+          information.<br />
+          <a href="https://lfprojects.org/policies/privacy-policy/">Privacy Policy</a> |
+          <a href="https://lfprojects.org/policies/terms-of-use/">Terms of Use</a>.
+        </section>
       </footer>
     );
   }
