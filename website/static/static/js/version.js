10aa553af4e9dce04c127c57e996bca7e6ae6743
Linsong Wang <wanglinsong@gmail.com>
2024-01-29 20:27:33 +0000

    Add 0.285.1 docs

--- website/static/static/js/version.js
+++ website/static/static/js/version.js
@@ -1,5 +1,5 @@
-const presto_latest_presto_version = '0.285';
-const presto_latest_num_commits = 204;
-const presto_latest_num_contributors = 53;
-const presto_latest_num_committers = 30;
-const presto_latest_date = 'January 14, 2024';
+const presto_latest_presto_version = '0.285.1';
+const presto_latest_num_commits = 4;
+const presto_latest_num_contributors = 3;
+const presto_latest_num_committers = 1;
+const presto_latest_date = 'January 29, 2024';
