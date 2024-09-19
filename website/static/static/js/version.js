e2807fffbb910364ebe65bf641e6551635317b55
Timothy Meehan <tim@timdmeehan.com>
2024-09-19 15:35:09 -0400

    Revert "Add 0.289 docs"
    This reverts commit 1f5c572906eb6f3fb894991e2c11a65b37ee1bcb.

--- website/static/static/js/version.js
+++ website/static/static/js/version.js
@@ -1,5 +1,5 @@
-const presto_latest_presto_version = '0.289';
+const presto_latest_presto_version = '0.288.1';
 const presto_latest_num_commits = 0;
 const presto_latest_num_contributors = 0;
 const presto_latest_num_committers = 0;
-const presto_latest_date = 'September 10, 2024';
+const presto_latest_date = 'August 12, 2024';
